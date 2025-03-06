fe790ad4fac394d29a2f8644c36cd5ac2c40e932
nithish0302 <nithi2005palani@gmail.com>
2025-03-06 20:54:27 +0530

    File added

---
+++ server.js
@@ -0,0 +1,9 @@
+// const { log } = require("console");
+
+console.log("Hi Hello");
+console.log("Welcome to Node JS");
+const os = require("os");
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
