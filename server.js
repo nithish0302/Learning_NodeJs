2c508f992446958a1937d0093272ff4f5cfabe87
nithish0302 <nithi2005palani@gmail.com>
2025-03-06 21:56:39 +0530

    Learned about the basic thigs in the node js

--- server.js
+++ server.js
@@ -2,8 +2,38 @@
 
 console.log("Hi Hello");
 console.log("Welcome to Node JS");
+
+console.log("---->");
+
 const os = require("os");
 
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
+
+console.log("---->");
+
+console.log(__dirname);
+console.log(__filename);
+
+console.log("---->");
+
+const path = require("path");
+console.log(path.dirname(__dirname));
+console.log(path.dirname(__filename));
+console.log(path.basename(__dirname));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename));
+
+console.log("---->");
+
+const { add, sub, mul, div, mod } = require("./math");
+
+console.log(`Addition ${add(5, 8)}`);
+console.log(`Subtraction ${sub(8, 5)}`);
+console.log(`Multiplication ${mul(3, 3)}`);
+console.log(`Division ${div(25, 5)}`);
+console.log(`Modulo ${mod(7, 8)}`);
+
+console.log("---->");
