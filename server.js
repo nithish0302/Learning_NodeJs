9a8f260176f3eeb8dc324b848bfbd13a99e79bf1
nithish0302 <nithi5palani@gmail.com>
2025-07-01 20:22:37 +0530

    Learning process started

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hi This is new Message that I am Started to Learning the NodeJs");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ rollno: "717822F136", name: "Nithish" });
+});
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log("The server has been started");
+});
